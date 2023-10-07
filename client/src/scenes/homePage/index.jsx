eea28b76015eedc8343a505eecdd53a2715486b3
Raj Singh <rajsinghast03@gmail.com>
2023-10-07 14:23:18 +0530

    Added Login Form

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
+function HomePage() {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+}
+
+export default HomePage;
